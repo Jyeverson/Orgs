780c7debcbfbbb68a267d7ca0fde79a2796bd88c
jyeverson <jyeversonsantos@gmail.com>
2022-06-17 13:18:24 -0300

    aplicando valores fora do xml

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -9,6 +10,13 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val nome = findViewById<TextView>(R.id.nome)
+        val descricao = findViewById<TextView>(R.id.descricao)
+        val valor = findViewById<TextView>(R.id.valor)
+
+        nome.text = "Cesta de Frutas"
+        descricao.text = "Laranja, manga e uva"
+        valor.text = "19.99"
     }
 
 }
