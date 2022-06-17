19823615e4fbb9ddcef53ea00486baa0b7172dd1
jyeverson <jyeversonsantos@gmail.com>
2022-06-17 16:06:27 -0300

    configurando activitys

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
