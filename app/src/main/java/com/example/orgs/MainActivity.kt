8eb6bcf46e2d7182d0332549da74178373f0d9e7
jyeverson <jyeversonsantos@gmail.com>
2022-06-17 11:52:02 -0300

    criando o xml

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo(a) ao ORGS", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
+
     }
 
 }
